a42018fb0cc3c58c26e361d38c78cd75c690f793
hamzsait <hamzasait@utexas.edu>
2021-05-03 11:59:27 -0500

    added User model outline

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const {Model, DataTypes} = require('Sequelize')
+const bcrypt = require('bycrypt')
+const sequelize = require("../config/connection")
+
+class User extends Model{
+    checkpassword(loginPw){
+        return bcrypt.compareSync(loginPw,this.password)
+    }
+}
+
+User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [6],
+        },
+      },
+      mineSweeperScore: {
+          type: DataTypes.INTEGER
+      }
+    },
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10)
+              return newUserData
+            },
+            beforeUpdate: async (updatedUserData) => {
+              updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10)
+              return updatedUserData
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'arcade_db',
+    });
+
+module.exports = User
