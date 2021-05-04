84a68a51f6df57092bdfb241d152484aabaa0a41
hamzsait <hamzasait@utexas.edu>
2021-05-04 15:08:24 -0500

    users

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -22,7 +22,7 @@ router.get('/clippy', async (req,res) =>{
 
 router.get("/login", async (req,res) =>{
   try{
-    res.render('clippy')
+    res.render('login')
   }
   catch (err){
     console.log(err)
