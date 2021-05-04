24a769cd016e83c4b3691a54418495c05fa1c463
hamzsait <hamzasait@utexas.edu>
2021-05-04 13:14:03 -0500

    added clippy

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,4 +10,24 @@ router.get('/', async (req, res) => {
     }
   });
 
+router.get('/clippy', async (req,res) =>{
+  try{
+    res.render('clippy')
+  }
+  catch(err){
+    console.log(err)
+    res.status(500).json(err)
+  }
+})
+
+router.get("/login", async (req,res) =>{
+  try{
+    res.render('clippy')
+  }
+  catch (err){
+    console.log(err)
+    res.status(500).json(err)
+  }
+})
+
 module.exports = router
