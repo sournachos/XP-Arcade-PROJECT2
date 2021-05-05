a9bd2a4e31373699166c650b82fcce528c1d9d90
sournachos <joserivera18100@gmail.com>
2021-05-05 11:06:25 -0500

    Fixed snake game

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const minesweeperRoutes = require('./minesweeperRoutes');
-const trollRoutes = require('./trollRoutes');
+const snakeRoutes = require('./snakeRoutes');
 const loginRoutes = require('./loginRoutes');
 
 router.use('/minesweeper', minesweeperRoutes);
-router.use('/troll', trollRoutes);
+router.use('/snake', snakeRoutes);
 router.use('/login', loginRoutes);
 module.exports = router;
