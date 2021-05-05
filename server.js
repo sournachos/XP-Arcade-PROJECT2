7a387d8c0312e1355fa9efaa2eb08fbc57442f1c
Mattjb97 <bohlmatthew@gmail.com>
2021-05-05 11:53:20 -0500

    userdata routes

--- server.js
+++ server.js
@@ -6,13 +6,14 @@ const routes = require('./controllers');
 //const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
+const { hash } = require('bcrypt');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ 
- // helpers 
+const hbs = exphbs.create({
+  // helpers 
 });
 
 const sess = {
@@ -39,3 +40,29 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
+
+
+
+const user = []
+
+
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+app.post('/users', async (req, res) => {
+  try {
+    const salt = bcrypt.getSalt()
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    console.log(salt)
+    console.log(hashedPassword)
+    const user = { name: req.body.name, password: req.body.passowrd }
+    users.push(user)
+    res.status(201).send()
+  }catch{
+    res.status(500).send()
+  }
+ 
+
+})
